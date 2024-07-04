70d863f78e35c5be75fffaea7220c75888d6d3cf
ACE <acechu2@gmail.com>
2024-07-04 17:16:41 +0800

    it's done

--- Singletons/disabledbuttons.gd
+++ Singletons/disabledbuttons.gd
@@ -1,3 +1,25 @@
 extends Node
 
-var disabled_buttons 
+var disabled_dictionary = {
+"Combat1A" : 1,
+"Combat1B" : 1,
+"Combat1C" : 1,
+"Forge2A" : 1, 
+"Random2B" : 1,
+"Forge2C" : 1,
+"Combat3A" : 1,
+"Combat3B" : 1,
+"FreeBeetle4A" : 1,
+"FreeItem4B" : 1,
+"FreeBeetle4C" : 1,
+"Random5A" : 1,
+"Forge5B" : 1,
+"Combat6A" : 1,
+"Combat6B" : 1,
+"FreeBeetle7A" : 1,
+"FreeItem7B" : 1,
+"Forge7C" : 1,
+"Combat8" : 1,
+}
+
+
